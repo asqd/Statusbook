15f1c1d585b382fc90d4a538a2c0d1b610d43348
asqd <asqd117@gmail.com>
2014-06-23 22:12:37 +0400

    Add validation tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
 
   has_many :statuses, dependent: :destroy
 
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+  
+  validates :profile_name, presence: true, 
+                          uniqueness: true,
+                          format: { with: /a-zA-z0-9_-/, 
+                          message: "Please use only letters, '-' or '_'"}
+
   def full_name
   	"#{first_name} #{last_name}"  	
   end
