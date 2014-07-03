67ce8cc055e47df3bc12faf838fcf44898ca15bb
asqd <asqd117@gmail.com>
2014-07-03 16:32:16 +0400

    Little fix

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find_by_profile_name(params[:id])
     if @user
-      @statuses = @user.statuses.paginate(page: params[:page], per_page: 5)
+      @statuses = @user.statuses.paginate(page: params[:page], per_page: 10)
       render action: :show
     else
       render 'public/404', status: 404, formats: [:html]
